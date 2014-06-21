19b3d244c2815275d435abc18e5c832a64ec51f8
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2014-06-21 09:31:26 +0900

    ・ライブラリ「gasha」の初期構成作成中。

--- proj/mk.sh
+++ proj/mk.sh
@@ -2,32 +2,42 @@
 
 #-------------------------------------------------------------------------------
 # mk.sh
-# 全プラットフォーム＆ビルド設定でメイクを実行
+# 全プラットフォーム＆ビルド設定でライブラリをメイク
 #
 # Gakimaru's researched and standard library for C++ - GASHA
 #   Copyright (c) 2014 Itagaki Mamoru
 #   Released under the MIT license
-#     https://github.com/gakimaru/gasha_proj/blob/master/LICENSE
+#     https://github.com/gakimaru/gasha_src/blob/master/LICENSE
 #-------------------------------------------------------------------------------
 
+# メイクファイル実行関数
+function run_makefile_core() {
+	echo '--------------------------------------------------------------------------------'
+	pushd $1
+	make $2
+	popd
+}
+
+# メイクファイル呼び出し
+function run_makefile() {
+	echo ''
+	echo '================================================================================'
+	echo $1, $2, $3
+	export BUILD_PLATFORM=$1
+	export BUILD_TYPE=$2
+	run_makefile_core . $3
+}
+
 # x86 & Relase
-export BUILD_PLATFORM=x86
-export BUILD_TYPE=Release
-make $1
+run_makefile x86 Release $1
 
 # x86 & Debug
-export BUILD_PLATFORM=x86
-export BUILD_TYPE=Debug
-make $1
+run_makefile x86 Debug $1
 
 # x64 & Relase
-export BUILD_PLATFORM=x64
-export BUILD_TYPE=Release
-#make $1
+#run_makefile x64 Release $1
 
 # x64 & Debug
-export BUILD_PLATFORM=x64
-export BUILD_TYPE=Debug
-#make $1
+#run_makefile x64 Debug $1
 
 # End of file
