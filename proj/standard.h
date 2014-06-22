9c6a8109b27e39f288ef074e87db6749fff82e78
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2014-06-22 11:48:28 +0900

    ・ソート／探索処理をライブラリに追加。

--- proj/standard.h
+++ proj/standard.h
@@ -8,7 +8,7 @@
 //
 // Gakimaru's researched and standard library for C++ - GASHA
 //   Copyright (c) 2014 Itagaki Mamoru
-//   Released under the MIT license
+//   Released under the MIT license.
 //     https://github.com/gakimaru/gasha_src/blob/master/LICENSE
 //--------------------------------------------------------------------------------
 
