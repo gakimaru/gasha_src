3193d96c7252bd61555eec7868e79607bb8f570e
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2014-07-08 10:49:19 +0900

    ・アロケータ追加中（一旦コミット）。 ・ビルド設定からビルド診断を分割。 ・ユーティリティから時間処理を分割。 ・基本算術からアラインメント計算を分割。

--- src/build_settings/build_settings_diag.cpp
+++ src/build_settings/build_settings_diag.cpp
@@ -1,6 +1,6 @@
 ﻿//--------------------------------------------------------------------------------
-// build_settings.cpp
-// ビルド設定【関数／実体定義部】
+// build_settings_diag.cpp
+// ビルド設定診断【関数／実体定義部】
 //
 // Gakimaru's researched and standard library for C++ - GASHA
 //   Copyright (c) 2014 Itagaki Mamoru
@@ -8,7 +8,7 @@
 //     https://github.com/gakimaru/gasha/blob/master/LICENSE
 //--------------------------------------------------------------------------------
 
-#include <gasha/build_settings/build_settings.inl>//ビルド設定【インライン関数／テンプレート関数定義部】
+#include <gasha/build_settings/build_settings_diag.inl>//ビルド設定【インライン関数／テンプレート関数定義部】
 
 GASHA_NAMESPACE_BEGIN;//ネームスペース：開始
 
@@ -16,22 +16,22 @@ GASHA_NAMESPACE_BEGIN;//ネームスペース：開始
 //ビルド設定
 
 //ライブラリビルド時の状態を診断
-const diagnosisModeForLibrary_t diagnosisModeForLibrary;
+const diagForLibrary_t diagForLibrary;
 
-//現在の状態を診断
-const diagnosisModeForCurrent_t diagnosisModeForCurrent;
+//現在のプロジェクトの状態を診断
+const diagForProject_t diagForProject;
 
 //----------------------------------------
 //ビルド設定が実行環境に適合するか診断
 //※特殊化
 template<>
-bool diagnoseBuildSettings<diagnosisModeForLibrary_t>(char* message, std::size_t& size, const diagnosisModeForLibrary_t mode)
+bool buildSettingsDiagnosticTest<diagForLibrary_t>(char* message, std::size_t& size, const diagForLibrary_t mode)
 {
-	return diagnoseBuildSettings<diagnosisModeForLibrary_internal_t>(message, size, diagnosisModeForLibrary_internal_t());
+	return buildSettingsDiagnosticTest<diagForLibrary_internal_t>(message, size, diagForLibrary_internal_t());
 }
 //※ライブラリビルド時の診断用関数を明示的にインスタンス化
 template
-bool diagnoseBuildSettings<diagnosisModeForLibrary_internal_t>(char* message, std::size_t& size, const diagnosisModeForLibrary_internal_t mode);
+bool buildSettingsDiagnosticTest<diagForLibrary_internal_t>(char* message, std::size_t& size, const diagForLibrary_internal_t mode);
 
 GASHA_NAMESPACE_END;//ネームスペース：終了
 
