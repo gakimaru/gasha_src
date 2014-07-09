a503c78bc24b78516a42d0553eaf49534e41c308
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2014-07-10 01:55:32 +0900

    ・ロックフリー双方向スタックアロケータを追加。

--- src/memory/allocator/lf_mono_allocator.cpp
+++ src/memory/allocator/lf_mono_allocator.cpp
@@ -33,47 +33,44 @@ GASHA_NAMESPACE_BEGIN;//ネームスペース：開始
 //メモリ確保
 void* lfMonoAllocator::alloc(const std::size_t size, const std::size_t align)
 {
-	if (m_size.load() > 0)//既にメモリ確保中なら失敗
-		return nullptr;
 	//サイズが0バイトならサイズを1に、アラインメントを0にする
 	//※要求サイズが0でも必ずメモリを割り当てる点に注意（ただし、アラインメントは守らない）
 	const std::size_t _size = size == 0 ? 1 : size;
 	const std::size_t _align = size == 0 ? 0 : align;
-	//サイズとアラインメントをチェック
-	char* new_ptr = adjustAlign(m_buffRef, _align);
-	const std::size_t padding_size = new_ptr - m_buffRef;
-	const size_type alloc_size = static_cast<size_type>(padding_size + _size);
-	if (alloc_size > m_maxSize)
+	while (m_size.load() == 0)
 	{
-	#ifdef GASHA_LF_MONO_ALLOCATOR_ENABLE_ASSERTION
-		static const bool NOT_ENOUGH_SPACE = false;
-		assert(NOT_ENOUGH_SPACE);
-	#endif//GASHA_LF_MONO_ALLOCATOR_ENABLE_ASSERTION
-		return nullptr;
-	}
-	
-	//使用中のサイズを更新
-	size_type now_size = 0;
-	if(!m_size.compare_exchange_strong(now_size, alloc_size))
-		return nullptr;//確保失敗（他のスレッドが割り込んでいる）
+		//サイズとアラインメントをチェック
+		char* new_ptr = adjustAlign(m_buffRef, _align);
+		const std::size_t padding_size = new_ptr - m_buffRef;
+		const size_type alloc_size = static_cast<size_type>(padding_size + _size);
+		if (alloc_size > m_maxSize)
+		{
+		#ifdef GASHA_LF_MONO_ALLOCATOR_ENABLE_ASSERTION
+			static const bool NOT_ENOUGH_SPACE = false;
+			assert(NOT_ENOUGH_SPACE);
+		#endif//GASHA_LF_MONO_ALLOCATOR_ENABLE_ASSERTION
+			return nullptr;
+		}
 
-	//空き領域を確保
-	return reinterpret_cast<void*>(new_ptr);
+		//使用中のサイズを更新
+		size_type now_size = 0;
+		if (m_size.compare_exchange_weak(now_size, alloc_size))
+		{
+			//空き領域を確保
+			return reinterpret_cast<void*>(new_ptr);
+		}
+	}
+	return nullptr;
 }
 
 //デバッグ情報作成
 std::size_t lfMonoAllocator::debugInfo(char* message)
 {
-#ifdef GASHA_HAS_DEBUG_FEATURE
 	std::size_t size = 0;
 	size += sprintf(message + size, "----- Debug Info for lfMonoAllocator -----\n");
 	size += sprintf(message + size, "buffRef=%p, maxSize=%d, size=%d, remain=%d, isAllocated=%s\n", m_buffRef, maxSize(), this->size(), remain(), toStr(isAllocated()));
 	size += sprintf(message + size, "----------\n");
 	return size;
-#else//GASHA_HAS_DEBUG_FEATURE
-	message[0] = '\0';
-	return 0;
-#endif//GASHA_HAS_DEBUG_FEATURE
 }
 
 //メモリ解放（共通処理）
